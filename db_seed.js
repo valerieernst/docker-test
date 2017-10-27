0dee2924d792562d0f54d3dc4c58ea9e8d098e4e
Valerie Ernst <valerie.ernst@hireathena.com>
2017-10-27 13:08:21 -0700

    Update to allow post requests

--- db_seed.js
+++ db_seed.js
@@ -1,17 +1,8 @@
-const db = require('./db_connection.js');
-
 // stand in for seeding data until i find more sustainable solution
-module.exports = () => {
-
-  db.connect(function(err){
-    if(err) {console.log('error with mysql db!', err);}
-    console.log('mysql db connected!');
-  });
-
+module.exports = (db) => {
   db.query('DROP DATABASE test');
   db.query('CREATE DATABASE test', (err) => { if(err) console.error(err); });
   db.query('USE test', (err) => {if (err) console.error(err); });
   db.query('CREATE TABLE user (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(20), last_name VARCHAR(20))', (err) => {if (err) console.error(err); });
   db.query('INSERT INTO user (first_name, last_name) VALUES("test", "testy")', (err) => {if (err) console.error(err); });
-
 }
--- server.js
+++ server.js
@@ -2,29 +2,39 @@
 const express    = require('express');
 const db_connection = require('./db_connection.js');
 const db_seed = require('./db_seed.js');
+const bodyParser = require('body-parser');
 
 const app = express();
-
-db_seed();
 
 db_connection.connect(function(err){
   if(err) {console.log('error with mysql db!', err);}
   console.log('mysql db connected!');
 });
 
+// this is a poor man's way to work through no data persistance...
+db_seed(db_connection);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('this is a test api!');
 });
 
 app.get('/users',(req,res) => {
-  db_connection.query('SELECT * from user LIMIT 2', function(err, rows, fields) {
+  db_connection.query('SELECT * from user LIMIT 2', (err, rows, fields) => {
   if (err) res.send(`there was a mysql error! here it is: ${err}`);
   res.send(rows);
   });
 });
 
-app.post('users', (req, res) => {
-
+app.post('/users', (req, res) => {
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  db_connection.query(`INSERT INTO user (first_name, last_name) VALUES (${firstName}, ${lastName})`, (err) => {
+    if (err) res.send(`there was a mysql error! here it is: ${err}`);
+    res.send('success!');
+  });
 });
 
 const port = process.env.PORT || 8080;
