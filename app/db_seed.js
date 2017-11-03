d4d7481a6c2171478611b2a01343d0db276b9aee
Valerie Ernst <valerie.ernst@hireathena.com>
2017-11-03 13:06:16 -0700

    Make db_seed more flexible

--- app/db_seed.js
+++ app/db_seed.js
@@ -1,7 +1,6 @@
 // stand in for seeding data until i find more sustainable solution
 module.exports = (db) => {
-  db.query('DROP DATABASE test');
-  db.query('CREATE DATABASE test', (err) => { if(err) console.error(err); });
+  db.query('CREATE DATABASE IF NOT EXISTS test');
   db.query('USE test', (err) => {if (err) console.error(err); });
-  db.query('CREATE TABLE user (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(20), last_name VARCHAR(20))', (err) => {if (err) console.error(err); });
+  db.query('CREATE TABLE IF NOT EXISTS user (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(20), last_name VARCHAR(20))', (err) => {if (err) console.error(err); });
 }
