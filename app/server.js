85e32fd415a230cb2ed58bef16f55dc250794131
Valerie Ernst <valerie.ernst@hireathena.com>
2017-11-06 17:02:43 -0800

    Remove cors stuff that is fixed by proxying requests because it makes security tighter

--- app/server.js
+++ app/server.js
@@ -17,21 +17,6 @@ db_seed(db_connection);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Add headers
-app.use(function (req, res, next) {
-    // todo: unhardcode this!
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3030');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
-});
-
 app.get('/', (req, res) => {
   res.send('this is a test api!');
 });
